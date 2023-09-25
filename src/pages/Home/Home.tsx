4fb835926a0bc713365a9449bbd5841dafd0a62c
Analsituation <analsituation@gmail.com>
2023-09-25 14:28:44 +0500

    skeleton for welcome gif

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
       <div className={styles.welcome}>
         <div>Huge welcome. All the functionality concentrated in products page. You need to login to get there.</div>
         {!loaded && (
-          <div className='my-8 mx-auto w-[640px] h-[476px] bg-[#5f8f7a] rounded-md overflow-hidden animate-pulse opacity-5'></div>
+          <div className='my-8 mx-auto w-full h-[220px] max-w-[640px] max-h-[476px] bg-[#9faa89] rounded-md overflow-hidden animate-pulse opacity-5'></div>
         )}
         <img src={welcome} alt='welcome' className={clsx(!loaded && 'hidden')} onLoad={() => setLoaded(true)} />
       </div>
