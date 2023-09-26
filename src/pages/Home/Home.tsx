113d88d8a7d26fef58f4d0374db2519bbb527f35
Analsituation <analsituation@gmail.com>
2023-09-27 02:59:24 +0500

    skeleton sizes fixed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,10 +13,8 @@ const Home = () => {
       <h3 className={styles.page_title}>Home page</h3>
       <div className={styles.welcome}>
         <div>Huge welcome. All the functionality concentrated in products page. You need to login to get there.</div>
-        {!loaded && (
-          <div className='my-8 mx-auto w-full h-[220px] max-w-[640px] max-h-[476px] bg-[#9faa89] rounded-md overflow-hidden animate-pulse opacity-5'></div>
-        )}
-        <img src={welcome} alt='welcome' className={clsx(!loaded && 'hidden')} onLoad={() => setLoaded(true)} />
+        {!loaded && <div className={styles.skeleton}></div>}
+        <img src={welcome} alt='welcome' className={clsx(!loaded && styles.hidden)} onLoad={() => setLoaded(true)} />
       </div>
     </div>
   )
